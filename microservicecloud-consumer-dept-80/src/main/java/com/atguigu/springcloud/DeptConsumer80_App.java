033f1328f5cb1a864fa5ef9f19eeb3d9a96e4b59
zyl <zhangyunlei393181024@gmail.com>
2019-04-17 18:56:50 +0800

    ribbon

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer80_App.java
@@ -1,15 +1,17 @@
 package com.atguigu.springcloud;
 
+import com.atguigu.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 
 @EnableEurekaClient
 @SpringBootApplication
 //在启动该微服务的时候就能去加载我们的自定义Ribbon配置类，从而使配置生效
-//@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration=MySelfRule.class)
-//@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration=MySelfRule.class)
+@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration= MySelfRule.class)
+//@RibbonClient(name="MICROSERVICECLOUD-DEPT")
 public class DeptConsumer80_App
 {
 	public static void main(String[] args)
